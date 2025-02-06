75b25e4d3e2dcd99905595ec4b02643b996228ec
K1tig <m603holden@gmail.com>
2025-02-06 09:42:40 -0500

    commit 0

---
+++ api.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"middleware"
+	"net/http"
+)
+
+type APIserver struct {
+	addr string
+}
+
+func NewAPIServer(addr string) *APIserver {
+	return &APIserver{
+		addr: addr,
+	}
+}
+
+func (s *APIserver) Run() error {
+	router := http.NewServeMux() //list routes below
+	router.HandleFunc("GET /races/{raceID}", func(w http.ResponseWriter, r *http.Request) {
+		raceID := r.PathValue("raceID")
+		w.Write([]byte("Race ID: " + raceID))
+	})
+
+	server := http.Server{
+		Addr:    s.addr,
+		Handler: middleware.Logging(router),
+	}
+	log.Println("server starting on:", s.addr)
+	return server.ListenAndServe()
+}
+
+func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("method: %s, path: %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	}
+}
